a4f107b0e5c2f72fb04007e4d7d636474141c35e
Pedro Augusto <pedroaugusto172015@gmail.com>
2026-06-22 21:40:17 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        MessageFormat.Escreva("Formatando a primeira mensagem.");
-        MessageFormat.Escreva("Formatando a segunda mensagem.");
+        MessageFormat.Escreva("Olá, Mundo!.");
+        MessageFormat.Escreva("Meu nome é, Pedro!.");
     }
 }
